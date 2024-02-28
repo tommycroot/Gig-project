85c80ade9bce7b7c98261f502feee6ab27da0aeb
TomCroot <thomascroot@hotmail.co.uk>
2024-02-28 16:56:45 +0000

    pipfile

--- gigproject/gigproject/settings.py
+++ gigproject/gigproject/settings.py
@@ -75,7 +75,7 @@ STATIC_URL = 'static/'
 CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'gigproject.urls'
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
