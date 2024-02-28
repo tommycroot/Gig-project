4a2efc1ecf6f09133363608c8e71785515a6c6ef
TomCroot <thomascroot@hotmail.co.uk>
2024-02-28 14:04:54 +0000

    ready to deploy

--- gigproject/gigproject/settings.py
+++ gigproject/gigproject/settings.py
@@ -62,16 +62,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://encoreapp.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'gigproject.urls'
